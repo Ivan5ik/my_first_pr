35839015cf8a082f1eb64694ec92a8ff2700a33b
Ivan5ik <lacoste545438@gmail.com>
2022-05-03 16:48:03 +0300

    created project

---
+++ src/First_Project/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+
+export default function First_Project() {
+  return (
+    <div>
+      hello
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
