2f69839072d6353009628cf44a4cba65d42db0ec
Ivan5ik <lacoste545438@gmail.com>
2022-05-03 17:24:34 +0300

    created input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import First_Project from "./First_Project";
+import { Button } from "antd";
+import moment from "moment";
+import Input from "./components/Input";
 
 function App() {
+  console.log(moment().format("dddd"));
+
   return (
     <div className="App">
-      <First_Project />
+      hello
+      <Button type="primary">Primary Button</Button>
+      <p>hello</p>
+      <Input />
     </div>
   );
 }
