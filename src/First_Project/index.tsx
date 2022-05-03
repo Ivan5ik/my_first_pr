af5315369f7d28264e025ea69639282b81643af3
Ivan5ik <96749360+Ivan5ik@users.noreply.github.com>
2022-05-03 17:12:39 +0300

    import moment (#1)

--- src/First_Project/index.tsx
+++ src/First_Project/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "antd/dist/antd.css";
 import { Button } from "antd";
+import moment from "moment";
 
 export default function First_Project() {
+  console.log(moment().format("dddd"));
+
   return (
     <div>
       hello
